25f3a1644f09725fd309079115f4c343d948962f
jakob4082 <114650116+jakob4082@users.noreply.github.com>
2022-11-14 09:18:09 +0100

    Update sketch.js

--- VSC p5js test/sketch.js
+++ VSC p5js test/sketch.js
@@ -1,11 +1,15 @@
 let bullets=[];
+let Player
 
 function setup() {
   createCanvas(400, 500);
+  Player = new Ship;
 }
 
 function draw() {
   background(220);
+  Player.move();
+  Player.display();
 
   if(random(0,100)<20){
     var bllt = new bullet(random(width),0,10)
@@ -22,4 +26,33 @@ function draw() {
     }
   }
 }
+
+  //player
+  class Ship{
+    constructor() {
+      this.x = 200;
+      this.y = 400;
+      this.diameter = 30;
+      this.speed = 5;
+    }
+  
+    move() {
+      if (keyIsPressed) {
+     if (keyCode == RIGHT_ARROW) {
+      this.x += 3; 
+     }  else if (keyCode == LEFT_ARROW) {
+       this.x -= 3;
+     } else if (keyCode == UP_ARROW) {
+       this.y -= 3; 
+     } else if (keyCode == DOWN_ARROW) {
+       this.y +=3; 
+     }
+    }
+    }
+  
+    display() {
+      ellipse(this.x, this.y, this.diameter, this.diameter);
+    }
+      
+    }
 
