ec0a5f0a1d95390fbac9afb90af6bd7b21ca8215
Hvemslev <andershvorslev@gmail.com>
2022-11-14 09:24:24 +0100

    hm

---
+++ VSC p5js test/plyrPrjctl.js
@@ -0,0 +1,14 @@
+class projectile{
+constructor(x,y){
+    this.x=x
+    this.y=y
+}
+show(){
+
+    
+}
+fire(){
+    this.y-=10
+}
+
+}
---
+++ VSC p5js test/shoot.js
@@ -0,0 +1,5 @@
+function shoot(){
+    if(keyIsDown(32)){
+        let shot = new projectile(playr.pos.x,playr.pos.y)
+    }
+}
