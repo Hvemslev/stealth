fd0655d158dc2d3ea5c70d764e3e3ed03a9342e4
Hvemslev <andershvorslev@gmail.com>
2022-11-14 08:50:33 +0100

    hm

--- VSC p5js test/sketch.js
+++ VSC p5js test/sketch.js
@@ -1,9 +1,24 @@
+let bullets=[];
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
-  circle(20,20,10);
-  rect(20,20,40,50);
+
+  if(random(0,100)<20){
+    var bllt = new bullet(random(width),0,10)
+    bullets.push(bllt)
+  }
+
+  //for(i=bullets.length-1;i<-1; i--){
+    for(i=0;i<bullets.length; i++){
+    bullets[i].show()
+    bullets[i].update()
+
+    if(bullets[i].sy>height){
+      bullets.splice(i,1)
+    }
+  }
 }
